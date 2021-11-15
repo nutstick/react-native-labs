4a9015be00d6a115c8447f0428b70edfaf1eb568
Nuttapat Kirawittaya <nuttapat.kirawittaya@gmail.com>
2021-11-15 13:56:02 +0800

    :art: split metro config for web

--- packages/storybook/metro.config.js
+++ packages/storybook/metro.config.js
@@ -6,15 +6,6 @@
  */
 const path = require("path");
 const fs = require("fs");
-const exclusionList = require("metro-config/src/defaults/exclusionList");
-
-const rnPath = fs.realpathSync(
-  path.resolve(require.resolve("react-native/package.json"), "..")
-);
-
-const assetRegistryPath = fs.realpathSync(
-  path.resolve(require.resolve('react-native-web/dist/modules/AssetRegistry/index.js'), '..'),
-)
 
 module.exports = {
   watchFolders: [path.resolve(__dirname, "../../")],
@@ -25,30 +16,9 @@ module.exports = {
         inlineRequires: true,
       },
     }),
-    assetRegistryPath,
   },
   resolver: {
-    platforms: ['web', 'ios', 'android', 'native'],
+    platforms: ['ios', 'android', 'native'],
     resolverMainFields: ["sbmodern", "browser", "main"],
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => {
-          if (name === "react-native") {
-            return path.join(__dirname, `node_modules/${name}-web`);
-          }
-          return path.join(__dirname, `node_modules/${name}`);
-        },
-      }
-    ),
-    blockList: exclusionList([
-      new RegExp(
-        `${(path.resolve(rnPath) + path.sep).replace(/[/\\\\]/g, "[/\\\\]")}.*`
-      ),
-      // This stops "react-native run-web" from causing the metro server to crash if its already running
-      new RegExp(
-        `${path.resolve(__dirname, "web").replace(/[/\\\\]/g, "[/\\\\]")}.*`
-      ),
-    ]),
   },
 };
